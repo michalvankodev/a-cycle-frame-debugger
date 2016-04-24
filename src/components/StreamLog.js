3f4aa56d5659e9c00a1260eeb25993ac0a3884ef
Michal Vanko <mklakakilli@gmail.com>
2016-04-24 10:06:42 +0200

    Added camera and a box to display values

--- src/components/StreamLog.js
+++ src/components/StreamLog.js
@@ -1,30 +1,27 @@
 import {h} from '@cycle/dom'
 import 'aframe-text-component'
 
-export default function StreamLog(logged$) {
-  const view$ = logged$.map(value =>
-    h(
-      'a-box',
-      {
+export default function StreamLog(value, position) {
+  return h(
+    'a-box',
+    {
+      attributes: {
+        depth: 0.1,
+        height: 2,
+        width: 5,
+        color: '#cccccc',
+        position: position,
+        'look-at': '#camera'
+      }
+    },
+    [
+      h('a-entity', {
         attributes: {
-          depth: 0.1,
-          height: 2,
-          width: 5,
-          color: '#cccccc'
+          position: '-2 0 0.2',
+          text: `text: ${value.toString()}; size: 0.3;`,
+          material: 'color: #000000'
         }
-      },
-      [
-        h('a-entity', {
-          attributes: {
-            position: '-2 0 0.2',
-            text: `text: ${value}; size: 0.3;`,
-            material: 'color: #000000'
-          }
-        })
-      ]
-    )
-  ).do(c => console.log(c))
-  return {
-    DOM: view$
-  }
+      })
+    ]
+  )
 }
